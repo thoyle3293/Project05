23b237f63feff517a1fca5962630fdb60a29c5dd
Travis <thoyle3293@gmail.com>
2017-08-25 12:30:14 -0700

    changes

---
+++ Data/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project05.Data
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Order = new HashSet<Order>();
+        }
+
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public ICollection<Order> Order { get; set; }
+    }
+}
